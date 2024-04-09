6e034780bb480000ed504164b420996499782f44
EDUARDO BORGES CAMBRAIA <eduardo_cambraia@estudante.sesisenai.org.br>
2024-04-09 10:49:54 -0300

    botão de atualizar feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable no-unused-vars */
 import './App.css'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
   const [livros, setLivros] = useState([]);
+  const [add, setAdd] = useState(true)
   const [novoLivro, setNovoLivro] = useState({
     isbn: '',
     titulo: '',
@@ -61,63 +63,120 @@ function App() {
       console.error('Erro ao atualizar o Livro:', error);
     }
   };
-              
+
   return (
-    <div>
+    < div >
       <h1>Gerenciamento de Livros</h1>
-  
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="isbn"
-          placeholder="Isbn"
-          value={novoLivro.isbn}
-          onChange={handleInputChange}
-        />
-         <input
-          type="text"
-          name="titulo"
-          placeholder="Titulo"
-          value={novoLivro.titulo}
-          onChange={handleInputChange}
-        />
-       
-        <input
-          type="text"
-          name="editora"
-          placeholder="Editora"
-          value={novoLivro.editora}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="autor"
-          placeholder="Autor"
-          value={novoLivro.autor}
-          onChange={handleInputChange}
-        />
-         <input
-          type="text"
-          name="genero"
-          placeholder="Gênero"
-          value={novoLivro.genero}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Adicionar o Livro</button>
-      </form>
-  
+      {
+        add ? (
+          <>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="isbn"
+                placeholder="Isbn"
+                value={novoLivro.isbn}
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                name="titulo"
+                placeholder="Titulo"
+                value={novoLivro.titulo}
+                onChange={handleInputChange}
+              />
+
+              <input
+                type="text"
+                name="editora"
+                placeholder="Editora"
+                value={novoLivro.editora}
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                name="autor"
+                placeholder="Autor"
+                value={novoLivro.autor}
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                name="genero"
+                placeholder="Gênero"
+                value={novoLivro.genero}
+                onChange={handleInputChange}
+              />
+              <button type="submit">Adicionar o Livro</button>
+            </form>
+          </>
+        ) : (
+          <>
+            <form >
+              <input
+                type="text"
+                name="isbn"
+                placeholder="Isbn"
+                value={novoLivro.isbn}
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                name="titulo"
+                placeholder="Titulo"
+                value={novoLivro.titulo}
+                onChange={handleInputChange}
+              />
+
+              <input
+                type="text"
+                name="editora"
+                placeholder="Editora"
+                value={novoLivro.editora}
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                name="autor"
+                placeholder="Autor"
+                value={novoLivro.autor}
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                name="genero"
+                placeholder="Gênero"
+                value={novoLivro.genero}
+                onChange={handleInputChange}
+              />
+              <button>Editar o Livro</button>
+
+
+            </form>
+          </>
+        )
+      }
+
+
+
       <ul>
         {livros.map((livro) => (
           <li key={livro.id}>
             {livro.isbn} - {livro.titulo} {livro.editora} {livro.autor} {livro.genero}
-            
+
             <button onClick={() => handleDelete(livro.id)}>Excluir</button>
-            
+
             <button
               onClick={() =>
                 handleUpdate(livro.id, {
                   ...livro,
-                  modelo: 'Livro atualizado Atualizado',
+                  isbn: novoLivro.isbn,
+                  titulo: novoLivro.titulo,
+                  editora: novoLivro.editora,
+                  autor: novoLivro.autor,
+                  genero: novoLivro.genero,
+
+
                 })
               }
             >
@@ -126,9 +185,9 @@ function App() {
           </li>
         ))}
       </ul>
-    </div>
+    </div >
   );
-  
+
 }
 
 export default App
